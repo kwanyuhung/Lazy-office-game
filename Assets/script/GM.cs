1975054d644b8a2131d2c5cccce09a1383ac622b
kwanyuhung <kwanyuhung@gmail.com>
2018-12-01 05:50:51 +0800

    final

---
+++ Assets/script/GM.cs
@@ -0,0 +1,303 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class GM : MonoBehaviour
+{
+
+    public int workmoney = 0;
+
+    public GameObject worker;
+
+    public GameObject outcomeUI;
+    public GameObject menuUI;
+    public GameObject gameEndUI;
+
+    public GameObject outcomeanimation;
+    public GameObject nextlevel;
+    public TextMeshProUGUI workmoney_text;
+
+
+    public List<GameObject> table;
+    public List<GameObject> ITDog;
+
+    public List<GameObject> totalITworker;
+    public int workercount = 1;
+
+    public List<GameObject> totalTable;
+    public int tablecount = 1;
+
+    public int workermoney = 1;
+    public int outcome = 20;
+    public float outcometimer = 0;
+    public int maxoutcomtime = 5;
+
+    bool gameEnd = false;
+
+    
+    public int maxgoal = 1000;
+    public float goaltimer = 0;
+    public int maxgoaltime = 15;
+
+    public int highermoney = 0;
+
+    public int[] ScoreBoard = new int[9];
+    public int issave = 0;
+
+    public TextMeshProUGUI Socrelist;
+
+    public void Update()
+    {
+        outcometimer += Time.deltaTime;
+        goaltimer += Time.deltaTime;
+        if (outcometimer > maxoutcomtime)
+        {
+            outcometimer = 0;
+            Removemoney(outcome);
+            outcomeanimation.SetActive(true);
+            outcomeanimation.GetComponent<TextMeshProUGUI>().text = "(-$" + outcome + ")";
+            outcomeanimation.GetComponent<Animation>().Play();
+        }
+
+        if (goaltimer >= maxgoaltime)
+        {
+            goaltimer = 0;
+            Nextstage();
+        }
+    }
+
+    public void FristTimeSave()
+    {
+        if (PlayerPrefs.GetInt("Save ed", issave) == 0)
+        {
+            Debug.Log("Not Save Yet");
+            for (int i = 0; i < ScoreBoard.Length; i++)
+            {
+                ScoreBoard[i] = 200;
+                PlayerPrefs.SetInt("socore" + i, ScoreBoard[i]);
+            }
+            issave = 1;
+            PlayerPrefs.SetInt("Save ed", issave);
+            PlayerPrefs.Save();
+        }
+        else
+        {
+            Debug.Log("save ed");
+            for (int i = 0; i < ScoreBoard.Length; i++)
+            {
+                ScoreBoard[i] = PlayerPrefs.GetInt("socore" + i, ScoreBoard[i]);
+            }
+        }
+    }
+
+    public void Start()
+    {
+        
+        FristTimeSave();
+        ITDog = new List<GameObject>(GameObject.FindGameObjectsWithTag("staff"));
+        table = new List<GameObject>(GameObject.FindGameObjectsWithTag("table"));
+        UpdateUI();
+    }
+
+    public void Updatemoney(int i)
+    {
+        workmoney += i;
+        if (workmoney > highermoney)
+        {
+            highermoney = workmoney;
+        }
+        UpdateUI();
+
+    }
+
+    public void Removemoney(int i)
+    {
+        workmoney -= i;
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        workmoney_text.text = "working = $" + workmoney;
+        if (workmoney < -200)
+        {//lose
+            Endgame();
+        }
+    }
+
+    public void Checktable(GameObject T, GameObject staff)
+    {
+        foreach (GameObject tableT in table)
+        {
+            if (tableT.GetComponent<tablefull>().isfull == false && tableT == T)
+            {
+                GameObject it = ITDog.Find(x => x == staff);
+                it.gameObject.GetComponent<StaffAI>().Needtowork(tableT);
+                it.gameObject.GetComponent<StaffAI>().table = tableT;
+                it.gameObject.GetComponent<StaffAI>().working = true;
+                tableT.GetComponent<tablefull>().isfull = true;
+                tableT.GetComponent<tablefull>().staff = staff;
+            }
+        }
+    }
+
+    public void Leavetable(GameObject staff)
+    {
+        foreach (GameObject tableT in table)
+        {
+            if (tableT.GetComponent<tablefull>().staff == staff)
+            {
+                tableT.GetComponent<tablefull>().staff = null;
+                tableT.GetComponent<tablefull>().isfull = false;
+            }
+        }
+    }
+
+    public GameObject Findfreetable()
+    {
+        foreach (GameObject tableT in table)
+        {
+            if (tableT.GetComponent<tablefull>().isfull == false)
+            {
+                return tableT;
+            }
+        }
+        return null;
+    }
+
+
+
+    public void Endgame()
+    {
+        gameEnd = true;
+        gameEndUI.SetActive(true);
+        gameEndUI.transform.GetChild(3).GetComponent<TextMeshProUGUI>().text = "Your max Score:" + highermoney;
+        SaveRecord(highermoney);
+
+        for(int i = 0;i < ScoreBoard.Length; i++)
+        {
+            Socrelist.text += (i + 1) + ")  " + ScoreBoard[i] +"\n";
+        }
+
+        Time.timeScale = 0;
+    }
+
+
+    public void Nextstage()
+    {
+        maxgoal *= 2;
+        outcome *= 2;
+        nextlevel.SetActive(true);
+        nextlevel.GetComponent<Animation>().Play();
+        outcomeUI.GetComponent<TextMeshProUGUI>().text = "Outcome:" + outcome + "$(5sec)";
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void Pause()
+    {
+        if (Time.timeScale != 0)
+        {
+            menuUI.SetActive(true);
+            //menuUI.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = "goal money:$" + maxgoal;
+            menuUI.transform.GetChild(2).GetComponent<TextMeshProUGUI>().text = "worker =" + workermoney + "($)";
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+            menuUI.SetActive(false);
+        }
+    }
+
+    public void SaveRecord(int higherscore)
+    {
+        for (int i = 0; i < ScoreBoard.Length; i++)
+        {
+            if (higherscore >= ScoreBoard[i])
+            {
+                Debug.Log("move down");
+                MoveDown(i, higherscore);
+                SaveData();
+                return;
+            }
+        }
+    }
+
+    void SaveData()
+    {
+        for (int i = 0; i < ScoreBoard.Length; i++)
+        {
+            PlayerPrefs.SetInt("socore" + i, ScoreBoard[i]);
+            PlayerPrefs.Save();
+        }
+    }
+
+    void MoveDown(int numberofscore, int score)
+    {
+        for (int i = ScoreBoard.Length-1; i >= 1; i--)
+        {
+            ScoreBoard[i] = ScoreBoard[i-1];
+        }
+        ScoreBoard[numberofscore] = score;
+    }
+
+    public void Upgrade()
+    {
+        if (workmoney >= 500)
+        {
+            Removemoney(500);
+            workermoney += 1;
+            menuUI.transform.GetChild(2).GetComponent<TextMeshProUGUI>().text = "worker =" + workermoney + "($)";
+        }
+    }
+
+    public void buyworker()
+    {
+        if (workercount < 4)
+        {
+            if (workmoney >= 45)
+            {
+                Removemoney(45);
+                foreach (GameObject worker in totalITworker)
+                {
+                    if (worker.activeSelf == false)
+                    {
+                        workercount += 1;
+                        worker.SetActive(true);
+                        ITDog.Add(worker);
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+    public void buytable()
+    {
+        if (tablecount < 4)
+        {
+            if (workmoney >= 100)
+            {
+                Removemoney(100);
+                foreach (GameObject Table in totalTable)
+                {
+                    if (Table.activeSelf == false)
+                    {
+                        tablecount += 1;
+                        Table.SetActive(true);
+                        table.Add(Table);
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
--- Assets/script/StaffAI.cs
+++ Assets/script/StaffAI.cs
@@ -4,34 +4,35 @@ using System.Collections;
 public class StaffAI : MonoBehaviour {
 
 
-	[HideInInspector]
-	public bool facingRight = true;
-	[HideInInspector]
+    [HideInInspector]
+    public bool facingRight = true;
+    [HideInInspector]
 
 
-	bool working = false;
-    public bool tableworking= false;
+    public bool working = false;
+    public bool tableworking = false;
 
     public GameObject table;
-	public Vector2 move;
-	public Vector2 stay;
-	public int range = 0;
-	public int range2 = 0;
-	public float number = 0;
-	public float speed  = 1.0f;
+    public Vector2 move;
+    public Vector2 stay;
+    public int range = 0;
+    public int range2 = 0;
+    public float number = 0;
+    public float speed = 1.0f;
 
-	public float time_i = 0.0f;
-	public float time_c = 3.0f;
+    public float time_i = 0.0f;
+    public float time_c = 3.0f;
 
-	public float time_a = 0.0f;
-	public float time_b;
+    public float time_a = 0.0f;
+    public float time_b;
 
-	private Animator anim;
+    private Animator anim;
 
     public GM GM;
 
     float moneytimer = 0;
     int earnmoney = 1;
+
 
     void Start()
     {
@@ -39,40 +40,43 @@ public class StaffAI : MonoBehaviour {
     }
 
 
-	void Update () {
-
-		if (working == false) {
-			if (Time.time > time_i) {
-				anim.Play ("character01_move");
-				time_i = Time.time + time_c;
-				range = Random.Range (-3, 3);
-				range2 = Random.Range (-3, 3);
-			}
-			AImove ();
-        }else{//working
+    void Update() {
+        earnmoney = GM.workermoney;
+        if (working == false) {
+            if (Time.time > time_i) {
+                anim.Play("character01_move");
+                time_i = Time.time + time_c;
+                range = Random.Range(-3, 3);
+                range2 = Random.Range(-3, 3);
+            }
+            AImove();
+        } else {//working
             if (ChecktimerScale())
             {
+                anim.Play("character 01");
                 moneytimer += Time.deltaTime;
                 GM.Updatemoney(earnmoney);
             }
         }
-		if (Time.time > time_a) {
-			time_a = Time.time+time_b;
-			working =false;
-            if(tableworking == true){
+        if (Time.time > time_a) {
+            time_a = Time.time + time_b;
+            working = false;
+            if (tableworking == true) {
+                anim.Play("character01_move");
                 print("leave");
                 tableworking = false;
                 GM.Leavetable(this.gameObject);
             }
-		}
-	}
+        }
+    }
 
-	void AImove (){
-			move = new Vector2 ((range) * Time.deltaTime, (range2) * Time.deltaTime);
-			transform.Translate (move);
-	}
+    void AImove() {
+        move = new Vector2((range) * Time.deltaTime, (range2) * Time.deltaTime);
+        transform.Translate(move);
+    }
 
-	void OnMouseDown(){
+    void OnMouseDown() {
+        
         if (ChecktimerScale())
         {
             GM.Checktable(GM.Findfreetable(), this.gameObject);
@@ -90,36 +94,52 @@ public class StaffAI : MonoBehaviour {
         }
         if (collision.gameObject.tag == "table")
         {
-            GM.Checktable(collision.gameObject, this.gameObject);
-            working = true;
+            if (working == false)
+            {
+                GM.Checktable(collision.gameObject, this.gameObject);
+                working = true;
+            }
         }
     }
-	
-	public void Needtowork(GameObject table){ //call by GM  // working
-		float position = table.transform.position.x+1.5f;
-		float position2 = table.transform.position.y+1;
 
-		stay = new Vector2 (position,position2);
-        this.transform.position =stay;
+    public void Needtowork(GameObject table) { //call by GM  // working
+        float position = table.transform.position.x + 1.5f;
+        float position2 = table.transform.position.y + 1;
 
-        anim.Play ("character 01");
+
+        stay = new Vector2(position, position2);
+        this.transform.position = stay;
+
+        
+
         tableworking = true;
-		callrandomshouldiworking();
+        callrandomshouldiworking();
+    }
 
-	}
+    void callrandomshouldiworking() {
+        time_b = Random.Range(1.0f, 6.0f);
+        time_a = Time.time + time_b;
+    }
 
-	void callrandomshouldiworking(){
-		time_b= Random.Range (1.0f, 6.0f);
-		time_a = Time.time + time_b;
-	}
-
-	void FixedUpdate () {
-		float h = range;
-		if (h < 0 && !facingRight)
-			flip ();
-		else if (h > 0 && facingRight)
-			flip ();
-	}
+    void FixedUpdate()
+    {
+        float h = range;
+        if (tableworking == false)
+        {
+            
+            if (h < 0 && !facingRight)
+                flip();
+            else if (h > 0 && facingRight)
+                flip();
+        }
+        else
+        {
+            if(this.transform.localScale.x < 0)
+            {
+                flip();
+            }
+        }
+    }
 
 	void flip(){
 		facingRight = !facingRight;
